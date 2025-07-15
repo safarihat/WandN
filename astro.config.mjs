940b194e619d29062f5231983ffb5cba28bc80aa
safarihat <safarihat@gmail.com>
2025-07-15 14:47:16 -0300

    don't know

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import github from "@astrojs/github-pages";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://safarihat.github.io",
+  site: "https://safarihat.github.io/WandN/",
+  base: "/WandN/",
+  output: "static",
+  adapter: github(),
   integrations: [mdx(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
